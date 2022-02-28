a36fe8cc1cf658dbf926e4da3707a356421d10e3
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:42:06 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_creating
+{
+    class Person
+    {
+        private string ans;
+        string surname;
+        string name;
+        string gender;
+        string status;
+        int age;
+        int salary;
+        public Person() { }
+        public Person(string Surname)
+        {
+            surname = Surname;
+        }
+        public Person(string Name, string Surname)
+        {
+            name = Name;
+            surname = Surname;
+        }
+        public string Surname
+        {
+            set { if (surname == null) surname = value; }
+            get { return surname; }
+        }
+        public string Name
+        {
+            set { if (name == null) name = value; }
+            get { return name; }
+        }
+        public string Gender
+        {
+            set { if (gender == null) gender = value; }
+            get { return gender; }
+        }
+        public int Age
+        {
+            set
+            {
+                age = value;
+                salary = value;
+                if (age < 7)
+                {
+                    status = "kid";
+                }
+                else if (age<17)
+                {
+                    status = "teenager";
+                }
+                else if (age < 24)
+                {
+                    status = "university student";
+                }
+                else
+                {
+                    Console.WriteLine("Do you have work?");
+                    ans = Console.ReadLine();
+                    ans = ans.ToLower();
+                    if (ans == "yes")
+                    {
+                        Console.WriteLine("What is your brutto salary?");
+                        salary = int.Parse(Console.ReadLine());
+                    }
+                    status = "adult";
+                }
+            }
+            get { return age; }
+        }
+        public string Status
+        {
+            get { return status; }
+        }
+        public int Salary
+        {
+            get { return salary; }
+        }
+        public void Greetings()
+        {
+            Console.WriteLine("Hello! My name is {0} and last name is {1}", name, surname);
+            Console.WriteLine("I'm a {0} years old {1}, and I'm a {2}", gender, age, status);
+        }
+        public void sth()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_creating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person female = new Person("Kask");
+            Console.WriteLine("How old are you?");
+            female.Age = int.Parse(Console.ReadLine());
+            female.Greetings();
+            //Person male = new Person();
+            //male.Surname = "Sugoma";
+            //male.Age = 3;
+            //Console.WriteLine(male.Surname, " is ", male.Age, " years old male");
+            //male.Greetings();
+            Person[] people = new Person[5];
+            for (int i = 0; i < 5; i++)
+            {
+                people[i] = new Person();
+                Console.WriteLine("What is your last name?");
+                people[i].Surname = Console.ReadLine();
+                Console.WriteLine("How old are you?");
+                people[i].Age = int.Parse(Console.ReadLine());
+            }
+            foreach (Person person in people)
+            {
+                person.Greetings();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
